c50bc182d27597b74438e2dac536c82689055952
Sachin T P. <sachin.tp@excelindia.com>
2024-11-05 10:30:04 +0530

    first next app created

---
+++ first-next-app/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-center font-bold text-6xl text-[#666666] flex justify-center items-center h-screen">
+      First Next.js app!
+    </div>
+  );
+}
